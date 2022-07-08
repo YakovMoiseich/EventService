2de1849a9ff9e7e6457ffedf5bb85e650dd0ff28
NETSRV\Fredgolm <fredgolm@deeplay.io>
2022-07-08 15:00:33 +0600

    removed unnecessary usings

--- Assets/Analytics/EventService.cs
+++ Assets/Analytics/EventService.cs
@@ -2,12 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using Assets.Analytics.EventsStorage;
 using Assets.Analytics.TimeProvider;
 using UnityEngine;
 using UnityEngine.Networking;
-using Random = System.Random;
 
 namespace Assets.Analytics
 {
